f67b78869382341cafcb4e2d8b39df2a3b6cbb2b
Anthony-Phiri-533-MW <anthonyphiri533@gmail.com>
2023-10-12 01:13:15 +0200

    added port to settings.py, changed port in manage.py to port in settings.py

--- manage.py
+++ manage.py
@@ -15,7 +15,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    port = getattr(sys.modules[__name__], "PORT",8000)
+    execute_from_command_line([sys.argv[0], "runserver", f"0.0.0.0:{port}"])
 
 
 if __name__ == '__main__':
